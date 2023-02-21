01e004793e18323dda4c1ba5906886a542d3411f
sierra0326 <65054528+sierra0326@users.noreply.github.com>
2023-02-21 17:39:06 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,14 @@
+//DOM references
+const redTurnText = document.querySelectorAll(".red-turn-text");
+const blueTurnText = document.querySelectorAll(".blue-turn-text");
+const HEIGHT = 7
+const WDITH = 6
+
+const board = [
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+    [ null, null, null, null, null, null, null ],
+];
